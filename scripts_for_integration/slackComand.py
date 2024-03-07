c5ff42d91bb308bb4ea79c3895dbff65551a52be
Muniib-Ali <muniib11@live.com>
2024-03-07 10:57:02 +0000

    script update

--- scripts_for_integration/slackComand.py
+++ scripts_for_integration/slackComand.py
@@ -1,14 +1,20 @@
 import slack
 from slack.errors import SlackApiError
 import mypsutil as cpu
+import json
 
 # Initialize the Slack client with your API token
 client = slack.WebClient(token='')
 
-# Function to send a direct message
-# Function to send a direct message
+with open("cpu.json", "r") as json_file:
+    cpu_data = json.load(json_file)
 
-
-
-client.chat_postMessage(channel='general', text='test');
-# Send the direct messag
+if 'muniib' in cpu_data and cpu_data['muniib']['CPU'] > 1.00:
+    # Send a message to the general channel
+    try:
+        response = client.chat_postMessage(channel='general', text='CPU usage for user muniib is over 1.00!')
+        print("Message sent successfully!")
+    except Exception as e:
+        print(f"Error sending message: {e}")
+else:
+    print("CPU usage for user muniib is not over 1.00 or user muniib doesn't exist.")
