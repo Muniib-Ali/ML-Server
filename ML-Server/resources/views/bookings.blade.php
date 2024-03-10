43bfccdf5ea2f54057ca44c6a0f1affcf26fbb73
Muniib-Ali <muniib11@live.com>
2024-03-10 15:09:47 +0000

    css for bookings page

--- ML-Server/resources/views/bookings.blade.php
+++ ML-Server/resources/views/bookings.blade.php
@@ -12,96 +12,99 @@
 
 </header>
 
-<body class = "bookings-page">
-  <div class="bookings-request">
-  <form class="bookings-form" action="/bookings" method="POST">
-      @csrf
-      <label for="resource_group">Select Resource Group:</label>
-      <select id="resource_group" name="resource_group" onchange="updateResources()">
-        @foreach ($resource_groups as $resource_group )
-        <option value="{{ $resource_group->id }}">{{$resource_group -> resource_group}}</option>
-        @endforeach
-      </select>
-      <label for="resource">Select Resource:</label>
-      <select id="resource" name="resource">
-
-      </select>
-      <label for="start">Start:</label>
-      <input type="date" id="start_date" name="start_date" required>
-      <select id="start_time" name="start_time">
-        <option value=00> 00 </option>
-        <option value=01> 01 </option>
-        <option value=02> 02 </option>
-        <option value=03> 03 </option>
-        <option value=04> 04 </option>
-        <option value=05> 05 </option>
-        <option value=06> 06 </option>
-        <option value=07> 07 </option>
-        <option value=08> 08 </option>
-        <option value=09> 09 </option>
-        <option value=10> 10 </option>
-        <option value=11> 11 </option>
-        <option value=12> 12 </option>
-        <option value=13> 13 </option>
-        <option value=14> 14 </option>
-        <option value=15> 15 </option>
-        <option value=16> 16 </option>
-        <option value=17> 17 </option>
-        <option value=18> 18 </option>
-        <option value=19> 19 </option>
-        <option value=20> 20 </option>
-        <option value=21> 21 </option>
-        <option value=22> 22 </option>
-        <option value=23> 23 </option>
-
-      </select>
-
-      <label for="end_day">End:</label>
-      <input type="date" id="end_date" name="end_date" required>
-
-      <select id="end_time" name="end_time">
-        <option value=00> 00 </option>
-        <option value=01> 01 </option>
-        <option value=02> 02 </option>
-        <option value=03> 03 </option>
-        <option value=04> 04 </option>
-        <option value=05> 05 </option>
-        <option value=06> 06 </option>
-        <option value=07> 07 </option>
-        <option value=08> 08 </option>
-        <option value=09> 09 </option>
-        <option value=10> 10 </option>
-        <option value=11> 11 </option>
-        <option value=12> 12 </option>
-        <option value=13> 13 </option>
-        <option value=14> 14 </option>
-        <option value=15> 15 </option>
-        <option value=16> 16 </option>
-        <option value=17> 17 </option>
-        <option value=18> 18 </option>
-        <option value=19> 19 </option>
-        <option value=20> 20 </option>
-        <option value=21> 21 </option>
-        <option value=22> 22 </option>
-        <option value=23> 23 </option>
-      </select>
-
-      <label for="notes"> Notes: </label>
-      <input type="text-box" placeholder="Notes" name="notes">
-
-      <button type=" submit">Submit</button>
-
-    </form>
-    
-  </div>
-
-
-
-  <div class = "calendar-container">
+<body>
+  <div class="bookings-page">
 
   <div id="calendar">
-  </div>
-  
+    </div>
+
+    <div class="bookings-form-container">
+      <form class="bookings-form" action="/bookings" method="POST">
+        @csrf
+        <label for="resource_group">Select Resource Group:</label>
+        <select id="resource_group" name="resource_group" onchange="updateResources()">
+          @foreach ($resource_groups as $resource_group )
+          <option value="{{ $resource_group->id }}">{{$resource_group -> resource_group}}</option>
+          @endforeach
+        </select>
+        <label for="resource">Select Resource:</label>
+        <select id="resource" name="resource">
+
+        </select>
+        <label for="start">Start:</label>
+        <input type="date" id="start_date" name="start_date" required>
+        <select id="start_time" name="start_time">
+          <option value=00> 00 </option>
+          <option value=01> 01 </option>
+          <option value=02> 02 </option>
+          <option value=03> 03 </option>
+          <option value=04> 04 </option>
+          <option value=05> 05 </option>
+          <option value=06> 06 </option>
+          <option value=07> 07 </option>
+          <option value=08> 08 </option>
+          <option value=09> 09 </option>
+          <option value=10> 10 </option>
+          <option value=11> 11 </option>
+          <option value=12> 12 </option>
+          <option value=13> 13 </option>
+          <option value=14> 14 </option>
+          <option value=15> 15 </option>
+          <option value=16> 16 </option>
+          <option value=17> 17 </option>
+          <option value=18> 18 </option>
+          <option value=19> 19 </option>
+          <option value=20> 20 </option>
+          <option value=21> 21 </option>
+          <option value=22> 22 </option>
+          <option value=23> 23 </option>
+
+        </select>
+
+        <label for="end_day">End:</label>
+        <input type="date" id="end_date" name="end_date" required>
+
+        <select id="end_time" name="end_time">
+          <option value=00> 00 </option>
+          <option value=01> 01 </option>
+          <option value=02> 02 </option>
+          <option value=03> 03 </option>
+          <option value=04> 04 </option>
+          <option value=05> 05 </option>
+          <option value=06> 06 </option>
+          <option value=07> 07 </option>
+          <option value=08> 08 </option>
+          <option value=09> 09 </option>
+          <option value=10> 10 </option>
+          <option value=11> 11 </option>
+          <option value=12> 12 </option>
+          <option value=13> 13 </option>
+          <option value=14> 14 </option>
+          <option value=15> 15 </option>
+          <option value=16> 16 </option>
+          <option value=17> 17 </option>
+          <option value=18> 18 </option>
+          <option value=19> 19 </option>
+          <option value=20> 20 </option>
+          <option value=21> 21 </option>
+          <option value=22> 22 </option>
+          <option value=23> 23 </option>
+        </select>
+
+        <label for="notes"> Notes: </label>
+        <input type="text-box" placeholder="Notes" name="notes">
+
+        <button type=" submit">Submit</button>
+
+      </form>
+
+
+    </div>
+
+
+
+
+
 
   </div>
 
@@ -116,7 +119,7 @@
         .then(response => response.json())
         .then(data => {
           data.forEach(resource => {
-            if(resource.is_enabled){
+            if (resource.is_enabled) {
               addOption(resourceSelect, resource.id, resource.name);
 
             }
@@ -171,7 +174,7 @@
             end: booking.end_date + 'T' + endTime,
           };
         }),
-        scrollTime: '00:00:00', 
+        scrollTime: '00:00:00',
         dayHeaderContent: function(info) {
           return info.date.toLocaleDateString('en-US', {
             weekday: 'long'
