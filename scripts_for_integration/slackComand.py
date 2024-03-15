a1737ba4b423a8f3efecebb405f59a17a4e4a2f4
Muniib-Ali <muniib11@live.com>
2024-03-15 12:28:13 +0000

    Bot update

--- scripts_for_integration/slackComand.py
+++ scripts_for_integration/slackComand.py
@@ -13,16 +13,20 @@ RESOURCE_GROUP = "arptracker4"
 while True:
 
     cpu_data = cpu.main()
-    bookings_data = bookings.fetch_bookings()
+    bookings_data = bookings.fetch_bookings(RESOURCE_GROUP)
     resources = bookings.fetch_resources()
     relevant_bookings = [booking for booking in bookings_data if booking['resource_group_name'] == RESOURCE_GROUP]
 
     users_to_exclude = ["root", "systemd+", "avahi", "message+", "syslog", "daemon", "xrdp", "nvidia-+", "rtkit", "colord", "kernoops"]
 
     for user, usage in cpu_data.items():
-        if user not in users_to_exclude and usage['CPU'] > resources.get(user) and user not in [booking['resource_name'] for booking in relevant_bookings]:
-            message = f"A user is using {user}: without booking and appointment! and threshold is {resources.get(user)}"
-
-            client.chat_postMessage(channel='U06J8L68WUA', text = message)
+        if user not in users_to_exclude:
+            if user in resources:  
+                if usage['CPU'] > resources[user] and user not in [booking['resource_name'] for booking in relevant_bookings]:
+                    message = f"A user is using {user} without a booking and appointment! CPU usage is above the threshold. Threshold is {resources[user]}"
+                    client.chat_postMessage(channel='U06J8L68WUA', text=message)
+            else:
+                message = f"The resource for user {user} has not been added to the website."
+                client.chat_postMessage(channel='U06J8L68WUA', text=message)
             
     time.sleep(60)
