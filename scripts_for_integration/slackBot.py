2cead8b28ba0c7e360735dbb31fc9a90c8181587
Muniib-Ali <muniib11@live.com>
2024-03-19 14:36:24 +0000

    updated bot

--- scripts_for_integration/slackBot.py
+++ scripts_for_integration/slackBot.py
@@ -3,7 +3,7 @@ import time
 import slack
 from slack.errors import SlackApiError
 import mypsutil as cpu
-import newBookingScript as bookings
+import websiteAPI as bookings
 import mygputil as gpu
 
 client = slack.WebClient(token='')
@@ -11,34 +11,11 @@ client = slack.WebClient(token='')
 RESOURCE_GROUP = "arptracker4"
 USER_MAPPINGS = {"muniib":"alimuniib@gmail.com"}
 WHITELIST = ["root", "systemd+", "avahi", "message+", "syslog", "daemon", "xrdp", "nvidia-+", "rtkit", "colord", "kernoops"]
-MINIMUM
 TIME_THRESHOLD = 600
+MINIMUM_USAGE = 10
 
 messages_sent = {}
 
-while False:
-    cpu_data = cpu.main()
-    bookings_data = bookings.fetch_bookings()
-    resources = bookings.fetch_resources(RESOURCE_GROUP)
-    relevant_bookings = [booking for booking in bookings_data if booking['resource_group_name'] == RESOURCE_GROUP]
-
-    users_to_exclude = ["root", "systemd+", "avahi", "message+", "syslog", "daemon", "xrdp", "nvidia-+", "rtkit", "colord", "kernoops"]
-
-    # {"cpu_block_1": (10,50), "cpu_block_2": (10, 50)}
-
-
-    print(resources)
-    for current_user, usage in cpu_data.items():
-        if current_user not in users_to_exclude:
-            if current_user in resources:  
-                if usage['CPU'] > resources[current_user] and current_user not in [booking['resource_name'] for booking in relevant_bookings]:
-                    message = f"A user is using {current_user} without a booking and appointment! CPU usage is above the threshold. Threshold is {resources[current_user]}"
-                    client.chat_postMessage(channel='U06J8L68WUA', text=message)
-            else:
-                message = f"The resource for user {current_user} has not been added to the website."
-                client.chat_postMessage(channel='U06J8L68WUA', text=message)
-            
-    time.sleep(60)
 
 
 def kill_pid(pid):
@@ -58,6 +35,8 @@ def discard_old_messages_sent(messages, threshold):
 def post_message(channel=None, text=None, messages=None):
     if channel is None or text is None or message is None:
         raise Exception("XXX")
+    if channel not in messages:
+        messages[channel] = []
     if text not in [x['text'] for x in messages[channel]]:
         client.chat_postMessage(channel=channel, text=text)
         messages[channel].append({'text': text, 'timestamp': time.time()})
@@ -68,11 +47,12 @@ while True:
     cpu_data = cpu.main()
     bookings_data = bookings.fetch_bookings()
     users_dict = bookings.fetch_users()
+    admin_users = [user for user in users_dict if user['is_admin'] == 1]
     relevant_bookings = [booking for booking in bookings_data if booking['resource_group_name'] == RESOURCE_GROUP]
     gpu_data = gpu.get_usage()
     
     for current_user, usage in cpu_data.items():
-        if current_user not in WHITELIST:
+        if current_user not in WHITELIST and usage['CPU'] > MINIMUM_USAGE:
             if current_user in USER_MAPPINGS:
                 email = USER_MAPPINGS.get(current_user, None)
                 slack_id = users_dict.get(email)
@@ -84,19 +64,22 @@ while True:
                     continue
                 else:
                     if upper_threshold_total != 0:
-                        message = f"A user: {current_user} is using more resources than they have booked. Threshold is {upper_threshold_total} and they are using {usage['CPU']}"
-                        post_message(channel='U06J8L68WUA', text=message, messages=messages_sent)
+                        for admin_user in admin_users:
+                            message = f"A user: {current_user} is using more resources than they have booked. Threshold is {upper_threshold_total} and they are using {usage['CPU']}"
+                            post_message(channel=admin_user.get('slack'), text=message, messages=messages_sent)
                         message = f"You are using more {RESOURCE_GROUP} : CPU than you have booked!"
                         post_message(channel=slack_id, text=message, messages=messages_sent)
                     elif upper_threshold_total == 0:
-                        message = f"A user: {current_user} is using {RESOURCE_GROUP}'s CPU without a booking!"
-                        post_message(channel='U06J8L68WUA', text=message, messages=messages_sent)
+                        for admin_user in admin_users:
+                            message = f"A user: {current_user} is using {RESOURCE_GROUP}'s CPU without a booking!"
+                            post_message(channel=admin_user.get('slack'), text=message, messages=messages_sent)
                         message = f"You are using {RESOURCE_GROUP} : CPU without having created a booking!"
                         post_message(channel=slack_id, text=message, messages=messages_sent)
 
             else:
-                message = f"User: {current_user} has not registered on the website"
-                post_message(channel='U06J8L68WUA', text=message, messages=messages_sent)
+                for admin_user in admin_users:
+                    message = f"User: {current_user} has not registered on the website and is using more than CPU minimum threshold"
+                    post_message(channel=admin_user.get('slack'), text=message, messages=messages_sent)
 
     
     for singleGPU, processes in gpu_data.items():
@@ -113,14 +96,16 @@ while True:
                     if hasBooked == True:
                         continue
                     else:
-                        message = f"A user: {current_user} is using {RESOURCE_GROUP}'s GPU number: {singleGPU} without a booking!"
-                        post_message(channel='U06J8L68WUA', text=message, messages=messages_sent)
+                        for admin_user in admin_users:
+                            message = f"A user: {process['user']} is using {RESOURCE_GROUP}'s GPU number: {singleGPU} without a booking!"
+                            post_message(channel=admin_user.get('slack'), text=message, messages=messages_sent)
                         message = f"You are using {RESOURCE_GROUP}'s GPU number: {singleGPU} without having created a booking!"
                         post_message(channel=slack_id, text=message, messages=messages_sent)
 
                 else:
-                    message = f"User: {current_user} has not registered on the website"
-                    post_message(channel='U06J8L68WUA', text=message, messages=messages_sent)
+                    for admin_user in admin_users:
+                        message = f"User: {process['user']} has not registered on the website and is using GPU number: {singleGPU}"
+                        post_message(channel=admin_user.get('slack'), text=message, messages=messages_sent)
 
     
     
--- scripts_for_integration/websiteAPI.py
+++ scripts_for_integration/websiteAPI.py
@@ -1,11 +1,7 @@
 import requests
-import json
-import os
 
 def fetch_bookings():
     url = "http://57.128.172.217:8000/get-currentbookings"
-
-    filepath = os.path.join('scripts_for_integration', 'bookings.json');
     response = requests.get(url)
     if response.status_code == 200:
         bookings_data = response.json()
