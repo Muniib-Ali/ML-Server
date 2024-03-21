611a691e7e40f4b0e6182ffb2d3063ff3c73d5d8
Muniib-Ali <muniib11@live.com>
2024-03-21 21:48:12 +0000

    CSS changes

--- ML-Server/resources/views/create-resources.blade.php
+++ ML-Server/resources/views/create-resources.blade.php
@@ -9,50 +9,54 @@
 
 </header>
 
-<body>
-    <div class="resource-group">
-        <form class="resource-group-form" action="/createresourcegroup" method="POST">
-            @csrf
-            <input type="text" placeholder="Resource group name" name="resource_group" required>
-
-
-            <button type="submit" id = "create-reasourcegroup-button"> Create resource group </button>
-
-        </form>
-    </div>
-    <div class="resource">
-        <form class="resource-form"  method="POST" action="/createresource">
-            @csrf
-            <label for="resource_group" id = "resource-group-label"> Resource group:</label>
-            <select name="resource_group" id = "resources-select">
-                <option value = "" disabled selected>Choose an option</option>
-                @foreach ($resource_group as $resource_group_member)
-                <option value="{{$resource_group_member->id}}">{{$resource_group_member->resource_group}}</option>
-                @endforeach
-            </select>
-            <input type="text" placeholder="Resource" name="name" required>
-
-            <input type = "number" min = "0" max = "100" placeholder="Lower threshold(CPU Only)" name  = "lThreshold">
-
+<body class="resources-body">
     
-            <input type = "number" min = "0" max = "100" placeholder="Upper threshold(CPU Only)" name  = "uThreshold">
+        <div class="resource-group">
+            <form class="resource-group-form" action="/createresourcegroup" method="POST">
+                @csrf
+                <input type="text" placeholder="Resource group name" name="resource_group" required>
 
 
-            <input type = "number" min = "0" max = "100" placeholder="Number(GPU Only)" name  = "number">
+                <button type="submit" id="create-reasourcegroup-button"> Create resource group </button>
 
-    
-            <input type="number" min="10" max="10000" placeholder="Credit cost(per hour)" name="value" required>
+            </form>
+        
+            <form class="resource-form" method="POST" action="/createresource">
+                @csrf
+                <label for="resource_group" id="resource-group-label"> Resource group:</label>
+                <select name="resource_group" id="resources-select">
+                    <option value="" disabled selected>Choose an option</option>
+                    @foreach ($resource_group as $resource_group_member)
+                    <option value="{{$resource_group_member->id}}">{{$resource_group_member->resource_group}}</option>
+                    @endforeach
+                </select>
+                <input type="text" placeholder="Resource" name="name" required>
+
+                <input type="number" min="0" max="100" placeholder="Lower threshold(CPU Only)" name="lThreshold">
+
+
+                <input type="number" min="0" max="100" placeholder="Upper threshold(CPU Only)" name="uThreshold">
+
+
+                <input type="number" min="0" max="100" placeholder="Number(GPU Only)" name="number">
+
+
+                <input type="number" min="10" max="10000" placeholder="Credit cost(per hour)" name="value" required>
 
 
 
-            <button type="submit" id = "create-reasource-button"> Create resource</button>
+                <button type="submit" id="create-reasource-button"> Create resource</button>
 
-        </form>
-    </div>
+            </form>
+
+            
+        </div>
+
+       
 
     <div class="change-resources">
-        <table class="table table-striped table-dark">
-                <thead>
+        <table class="table table-striped table-dark" id = "resource-table">
+            <thead>
                 <tr>
                     <th>Resource Group</th>
                     <th>Resource</th>
@@ -61,8 +65,8 @@
                     <th>Delete</th>
                 </tr>
 
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 @foreach ($resources as $resource )
                 <tr>
                     <td>{{$resource-> resource_group_name}}</td>
@@ -71,19 +75,19 @@
                     <td>
                         <form action="admin/resource/change-status/{{$resource->id}}" method="post">
                             @csrf
-                            <button type="submit" id ="change-resource-status">{{$resource->is_enabled? 'Disable' : 'Enable'}}</button>
+                            <button type="submit" id="change-resource-status">{{$resource->is_enabled? 'Disable' : 'Enable'}}</button>
                         </form>
 
                     </td>
                     <td>
                         <form action="admin/resource/delete/{{$resource->id}}" method="post">
                             @csrf
-                            <button type="submit" id = "resource-delete-button">Delete</button>
+                            <button type="submit" id="resource-delete-button">Delete</button>
                         </form>
                     </td>
                 </tr>
                 @endforeach
-                </tbody>
+            </tbody>
         </table>
     </div>
 </body>
